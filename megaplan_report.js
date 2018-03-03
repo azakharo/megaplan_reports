c0453ead61339544cdf197a850cebf204a6cd78b
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2018-03-03 08:37:35 +0300

    ask for time period if not specified.

--- megaplan_report.js
+++ megaplan_report.js
@@ -25,8 +25,8 @@ async function main() {
     .option('-s, --server [server]', 'Server URL, like \<name\>.megaplan.ru')
     .option('-u, --user [user]', 'Username')
     .option('-p, --password [password]', 'Password. If not specified, a user will be asked for it.')
-    .option('--start [start]', `Time period start in format '${INPUT_DATE_FRMT}'. If omitted, the beginning of currect month is used.`)
-    .option('--end [end]', `Time period end in format '${INPUT_DATE_FRMT}'. If omitted, current date is used.`)
+    .option('--start [start]', `Time period START in format '${INPUT_DATE_FRMT}'`)
+    .option('--end [end]', `Time period END in format '${INPUT_DATE_FRMT}'`)
     .option('-o, --outdir [outdir]', 'Directory to place the report into. If not specified, the current working directory is used.')
     .parse(process.argv);
 
@@ -61,24 +61,65 @@ async function main() {
     }
   }
 
-  let dtStart = moment().startOf('month');
-  let dtEnd = moment();
+  let dtStart = null;
   if (program.start) {
     dtStart = moment(program.start, INPUT_DATE_FRMT, true);
     if (!dtStart.isValid()) {
       log(chalk.red(`Invalid time period START: ${program.start}`));
       exit(1);
     }
-    dtStart = dtStart.startOf('day');
   }
+  else {
+    const questions = [{
+      type: 'input',
+      name: 'start',
+      message: `Time period START in format '${INPUT_DATE_FRMT}' (leave blank for the beginning of currect month): `
+    }];
+
+    const answers = await prompt(questions);
+    if (!answers.start) {
+      dtStart = moment().startOf('month');
+    }
+    else {
+      dtStart = moment(answers.start, INPUT_DATE_FRMT, true);
+      if (!dtStart.isValid()) {
+        log(chalk.red(`Invalid time period START: ${answers.start}`));
+        exit(1);
+      }
+    }
+  }
+  dtStart = dtStart.startOf('day');
+
+  let dtEnd = null;
   if (program.end) {
     dtEnd = moment(program.end, INPUT_DATE_FRMT, true);
     if (!dtEnd.isValid()) {
       log(chalk.red(`Invalid time period END: ${program.end}`));
       exit(1);
     }
-    dtEnd = dtEnd.endOf('day');
   }
+  else {
+    const questions = [{
+      type: 'input',
+      name: 'end',
+      message: `Time period END in format '${INPUT_DATE_FRMT}' (leave blank for today): `
+    }];
+
+    const answers = await prompt(questions);
+    if (!answers.end) {
+      dtEnd = moment();
+    }
+    else {
+      dtEnd = moment(answers.end, INPUT_DATE_FRMT, true);
+      if (!dtEnd.isValid()) {
+        log(chalk.red(`Invalid time period END: ${answers.end}`));
+        exit(1);
+      }
+    }
+  }
+  dtEnd = dtEnd.endOf('day');
+
+  // Validate time period
   if (dtStart.isSameOrAfter(dtEnd)) {
     log(chalk.red(`Invalid time period: ${getTimePeriodStr(dtStart, dtEnd)}`));
     exit(1);
