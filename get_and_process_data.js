9806f414f135d89b5bd3e53e15bf99e2dcb97855
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2018-03-10 16:38:52 +0300

    getting data: rem datetime filtering when getting projects and tasks.

--- get_and_process_data.js
+++ get_and_process_data.js
@@ -12,23 +12,19 @@ module.exports = async function getReportData(mpClient, dtStart, dtEnd) {
 
   const allProjects = await getProjects(mpClient);
   log(`Loaded ${allProjects.length} projects`);
-  // Filter projects by start, end
-  const projects = filter(allProjects, prj => filterByStartEnd(prj, dtStart, dtEnd));
-  log(`Projects after filtering: ${projects.length}`);
-  // for (const prj of projects) {
-  //   logData(prj);
-  // }
+  const projects = allProjects;
+  // // Filter projects by start, end
+  // const projects = filter(allProjects, prj => filterByStartEnd(prj, dtStart, dtEnd));
+  // log(`Projects after filtering: ${projects.length}`);
 
   const allTasks = await getTasks(mpClient);
   log(`Loaded ${allTasks.length} tasks`);
   // TODO tasks detailed or not?
   // TODO request tasks with filter?
-  // Filter tasks by start, end
-  let tasks = filter(allTasks, task => filterByStartEnd(task, dtStart, dtEnd));
-  log(`Tasks after filtering by start/end time: ${tasks.length}`);
-  // for (const task of tasks) {
-  //   logData(task);
-  // }
+  let tasks = allTasks;
+  // // Filter tasks by start, end
+  // let tasks = filter(allTasks, task => filterByStartEnd(task, dtStart, dtEnd));
+  // log(`Tasks after filtering by start/end time: ${tasks.length}`);
 
   // Get comments per task
   log('Loading comments...');
