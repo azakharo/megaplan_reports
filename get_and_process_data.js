891a157ed7918a3bba35ad56d78c874b25331c54
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2018-03-13 15:01:49 +0300

    commented some code to experiment with projects.

--- get_and_process_data.js
+++ get_and_process_data.js
@@ -13,92 +13,93 @@ module.exports = async function getReportData(mpClient, dtStart, dtEnd) {
   const allProjects = await getProjects(mpClient);
   log(`Loaded ${allProjects.length} projects`);
   const projects = allProjects;
+  logData(projects);
   // // Filter projects by start, end
   // const projects = filter(allProjects, prj => filterByStartEnd(prj, dtStart, dtEnd));
   // log(`Projects after filtering: ${projects.length}`);
 
-  const allTasks = await getTasks(mpClient);
-  log(`Loaded ${allTasks.length} tasks`);
-  // TODO tasks detailed or not?
-  // TODO request tasks with filter?
-  let tasks = allTasks;
-  // // Filter tasks by start, end
-  // let tasks = filter(allTasks, task => filterByStartEnd(task, dtStart, dtEnd));
-  // log(`Tasks after filtering by start/end time: ${tasks.length}`);
-
-  // Get comments per task
-  log('Loading comments...');
-  for (const [taskInd, task ] of tasks.entries()) {
-    let allComments = null;
-    try {
-      allComments = await getComments(mpClient, task.id);
-    }
-    catch (e) {
-      log(chalk.red(stringify(e)));
-      exit(2);
-    }
-
-    // Filter comments
-    const commentsFiltered = filter(allComments, c => {
-      const dt = moment(c.time_created);
-      return c.work && (dt.isSameOrAfter(dtStart) && dt.isSameOrBefore(dtEnd));
-    });
-
-    log(`task ${taskInd + 1}/${tasks.length}: loaded ${allComments.length} comments, after filtering ${commentsFiltered.length}`);
-
-    // Associate comments with task
-    task.comments = commentsFiltered;
-  }
-
-  // Remove tasks with no comments
-  const tasksBeforeCommentFilterCount = tasks.length;
-  tasks = filter(tasks, t => t.comments.length > 0);
-  const tasksWithCommentsCount = tasks.length;
-  if (tasksWithCommentsCount !== tasksBeforeCommentFilterCount) {
-    log(`Found ${tasksBeforeCommentFilterCount - tasksWithCommentsCount} tasks w/out comments. They are ignored.`);
-    log(`Tasks remaining after all the filtering: ${tasks.length}`);
-  }
-
-  log('Calculate work per task');
-  for (const task of tasks) {
-    calcTaskWork(task);
-  }
-
-  log('Calculate work per project');
-  projects.forEach(proj => {
-    proj.tasks = filter(tasks, t => t.project.id === proj.id);
-    proj.totalWork = reduce(proj.tasks, (total, task) => (total + task.totalWork), 0);
-  });
-
-  log('Calculate work per employee (total)');
-  employees.forEach(empl => {
-    empl.totalWork = 0;
-    tasks.forEach(task => {
-      const taskWork = task.employee2work[empl.id];
-      if (taskWork) {
-        empl.totalWork += taskWork;
-      }
-    });
-  });
-
-  log('Calculate employee work per project');
-  employees.forEach(empl => {
-    empl.proj2work = {};
-    projects.forEach(proj => {
-      empl.proj2work[proj.id] =
-        reduce(proj.tasks, (total, task) => (total + (task.employee2work[empl.id] || 0)), 0);
-    });
-  });
-
-  const totalTotal = reduce(projects, (total, proj) => (total + proj.totalWork), 0);
-  log(`TOTAL work for the specified period: ${totalTotal} minutes OR ${(totalTotal / 60).toFixed(1)} hours`);
-
-  return {
-    employees,
-    projects,
-    tasks,
-    totalTotal
-  };
+  // const allTasks = await getTasks(mpClient);
+  // log(`Loaded ${allTasks.length} tasks`);
+  // // TODO tasks detailed or not?
+  // // TODO request tasks with filter?
+  // let tasks = allTasks;
+  // // // Filter tasks by start, end
+  // // let tasks = filter(allTasks, task => filterByStartEnd(task, dtStart, dtEnd));
+  // // log(`Tasks after filtering by start/end time: ${tasks.length}`);
+  //
+  // // Get comments per task
+  // log('Loading comments...');
+  // for (const [taskInd, task ] of tasks.entries()) {
+  //   let allComments = null;
+  //   try {
+  //     allComments = await getComments(mpClient, task.id);
+  //   }
+  //   catch (e) {
+  //     log(chalk.red(stringify(e)));
+  //     exit(2);
+  //   }
+  //
+  //   // Filter comments
+  //   const commentsFiltered = filter(allComments, c => {
+  //     const dt = moment(c.time_created);
+  //     return c.work && (dt.isSameOrAfter(dtStart) && dt.isSameOrBefore(dtEnd));
+  //   });
+  //
+  //   log(`task ${taskInd + 1}/${tasks.length}: loaded ${allComments.length} comments, after filtering ${commentsFiltered.length}`);
+  //
+  //   // Associate comments with task
+  //   task.comments = commentsFiltered;
+  // }
+  //
+  // // Remove tasks with no comments
+  // const tasksBeforeCommentFilterCount = tasks.length;
+  // tasks = filter(tasks, t => t.comments.length > 0);
+  // const tasksWithCommentsCount = tasks.length;
+  // if (tasksWithCommentsCount !== tasksBeforeCommentFilterCount) {
+  //   log(`Found ${tasksBeforeCommentFilterCount - tasksWithCommentsCount} tasks w/out comments. They are ignored.`);
+  //   log(`Tasks remaining after all the filtering: ${tasks.length}`);
+  // }
+  //
+  // log('Calculate work per task');
+  // for (const task of tasks) {
+  //   calcTaskWork(task);
+  // }
+  //
+  // log('Calculate work per project');
+  // projects.forEach(proj => {
+  //   proj.tasks = filter(tasks, t => t.project.id === proj.id);
+  //   proj.totalWork = reduce(proj.tasks, (total, task) => (total + task.totalWork), 0);
+  // });
+  //
+  // log('Calculate work per employee (total)');
+  // employees.forEach(empl => {
+  //   empl.totalWork = 0;
+  //   tasks.forEach(task => {
+  //     const taskWork = task.employee2work[empl.id];
+  //     if (taskWork) {
+  //       empl.totalWork += taskWork;
+  //     }
+  //   });
+  // });
+  //
+  // log('Calculate employee work per project');
+  // employees.forEach(empl => {
+  //   empl.proj2work = {};
+  //   projects.forEach(proj => {
+  //     empl.proj2work[proj.id] =
+  //       reduce(proj.tasks, (total, task) => (total + (task.employee2work[empl.id] || 0)), 0);
+  //   });
+  // });
+  //
+  // const totalTotal = reduce(projects, (total, proj) => (total + proj.totalWork), 0);
+  // log(`TOTAL work for the specified period: ${totalTotal} minutes OR ${(totalTotal / 60).toFixed(1)} hours`);
+  //
+  // return {
+  //   employees,
+  //   projects,
+  //   tasks,
+  //   totalTotal
+  // };
 };
 
 function calcTaskWork(task) {
