accf4da40a641a0507856d0b246eed6760fc16a3
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2018-03-30 14:38:19 +0300

    Revert "Get all tasks (rem any filtering)."
    This reverts commit cb4aae7cf387c370b1ab5aa5358a0738ae1458c2.

--- get_and_process_data.js
+++ get_and_process_data.js
@@ -41,7 +41,7 @@ module.exports = async function getReportData(mpClient, dtStart, dtEnd, projectF
     proj.comments = commentsFiltered;
   }
 
-  const allTasks = await getTasks(mpClient);
+  const allTasks = await getTasks(mpClient, dtStart.subtract(2, 'months'));
   log(`Loaded ${allTasks.length} tasks`);
   // Filter tasks by start, end
   let tasks = filter(allTasks, task => filterTaskByEnd(task, dtEnd));
